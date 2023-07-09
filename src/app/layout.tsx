bafb01fdb65fe06c26356d5781adf14ec257c279
Lagbalu Lateef <chelseg14@gmail.com>
2023-07-09 13:55:13 +0100

    feat: added head tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.scss";
 import { Poppins, Space_Grotesk } from "next/font/google";
 import "swiper/css";
 import "swiper/css/pagination";
+import Head from "next/head";
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -31,10 +32,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-      />
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+      </Head>
+
       <body className={`${space_grotesk.variable} ${poppins.variable} `}>
         {children}
         <Footer />
